270b0509cf2a4a4f4c6540ad8c31255f4ad552c4
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2018-11-14 17:38:59 -0300

    Branch eclipse!

--- Git-Teste-20182/src/HelloWorld.java
+++ Git-Teste-20182/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World !!!");
 		
-		System.out.println("Eclipse change!!!");
+		System.out.println("Eclipse change!!! [branch eclipse]");
 
 		System.out.println("Bash change! ");
 	}
